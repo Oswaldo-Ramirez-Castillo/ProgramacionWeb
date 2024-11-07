0e07ce70e525a3d28e188023a3facbd766554806
Oswaldo <duiltheoz98@gmail.com>
2024-11-07 12:36:48 -0600

    Hola mundo express

---
+++ Parcial3/Hello World/HolaMundo.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+});
+
+app.listen(8082, () => {
+    console.log('Servidor Express escuchando en puerto 8082');
+});
+
